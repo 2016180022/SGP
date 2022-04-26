d6e256be3df4eb599c6d0000d7ef8fd98c559bd6
2016180022 <71115355+2016180022@users.noreply.github.com>
2022-04-26 18:59:52 +0900

    게임 타일 출력

--- DND/app/src/main/java/kr/ac/tukorea/ge/n2016180022/dnd/framework/UpperView.java
+++ DND/app/src/main/java/kr/ac/tukorea/ge/n2016180022/dnd/framework/UpperView.java
@@ -10,14 +10,23 @@ import android.util.AttributeSet;
 import android.view.View;
 
 import androidx.annotation.Nullable;
+import androidx.lifecycle.SavedStateHandle;
 
 import kr.ac.tukorea.ge.n2016180022.dnd.R;
 
 public class UpperView extends View {
 
     private Bitmap bgUpper;
+    private Bitmap gameTile;
     private Rect upperSrcRect = new Rect();
     private Rect upperDstRect = new Rect();
+    private Rect tileSrcRect = new Rect();
+    private Rect tileDstRect = new Rect();
+    private int STATE;
+
+    public final int GAMESTATE = 1;
+    public final int CHARSTATE = 2;
+    public final int EQUSTATE = 3;
 
     public UpperView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
@@ -26,15 +35,50 @@ public class UpperView extends View {
 
     void initView() {
         Resources res = getResources();
+
         bgUpper = BitmapFactory.decodeResource(res, R.mipmap.info_box);
         upperSrcRect.set(0, 0, bgUpper.getWidth(), bgUpper.getHeight());
+
+        gameTile = BitmapFactory.decodeResource(res, R.mipmap.tile_town1);
+        tileSrcRect.set(0, 0, gameTile.getWidth(), gameTile.getHeight());
+
+        STATE = GAMESTATE;
 
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         //super.onDraw(canvas);
+        drawBackground(canvas);
+        switch(STATE)
+        {
+            case GAMESTATE:
+                drawGame(canvas);
+                break;
+            case CHARSTATE:
+                drawChar();
+                break;
+            case EQUSTATE:
+                drawEqu();
+                break;
+        }
+    }
+
+    private void drawGame(Canvas canvas) {
+        tileDstRect.set(0, 0, canvas.getWidth(), canvas.getHeight());
+        canvas.drawBitmap(gameTile, tileSrcRect, tileDstRect, null);
+    }
+
+    private void drawChar() {
+
+    }
+
+    private void drawEqu() {
+
+    }
+
+    private void drawBackground(Canvas canvas) {
         upperDstRect.set(0, 0, canvas.getWidth(), canvas.getHeight());
         canvas.drawBitmap(bgUpper, upperSrcRect, upperDstRect, null);
-}
+    }
 }
