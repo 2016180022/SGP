e68cb9cef99ee760da2604ea3f9a77b1918d8560
2016180022 <71115355+2016180022@users.noreply.github.com>
2022-04-26 18:21:39 +0900

    액티비티 연결, 버튼 기능 구현

---
+++ DND/app/src/main/java/kr/ac/tukorea/ge/n2016180022/dnd/app/GameActivity.java
@@ -0,0 +1,16 @@
+package kr.ac.tukorea.ge.n2016180022.dnd.app;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import kr.ac.tukorea.ge.n2016180022.dnd.R;
+
+public class GameActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+}
--- DND/app/src/main/java/kr/ac/tukorea/ge/n2016180022/dnd/app/MainActivity.java
+++ DND/app/src/main/java/kr/ac/tukorea/ge/n2016180022/dnd/app/MainActivity.java
@@ -2,17 +2,48 @@ package kr.ac.tukorea.ge.n2016180022.dnd.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import kr.ac.tukorea.ge.n2016180022.dnd.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnStart;
+    private Button btnChar;
+    private Button btnEqu;
+    private Button btnQuit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initView();
+    }
+
+    private void initView() {
+        btnStart = findViewById(R.id.startButton);
+        btnChar = findViewById(R.id.charButton);
+        btnEqu = findViewById(R.id.equButton);
+        btnQuit = findViewById(R.id.quitButton);
+    }
+
+    public void OnClick(View view) {
+        switch(view.getId())
+        {
+            case R.id.startButton:
+                break;
+            case R.id.charButton:
+                break;
+            case R.id.equButton:
+                break;
+        }
+
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
     }
 
 }
