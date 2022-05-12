ba1f31d897fb335c67a9ceee1af211b71fd25aca
2016180022 <71115355+2016180022@users.noreply.github.com>
2022-05-12 14:01:44 +0900

    activity 동작하게 수정

--- DungeonNDeffence/app/src/main/java/kr/ac/tukorea/ge/n2016180022/dungeonndeffence/app/MainActivity.java
+++ DungeonNDeffence/app/src/main/java/kr/ac/tukorea/ge/n2016180022/dungeonndeffence/app/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import kr.ac.tukorea.ge.n2016180022.dungeonndeffence.R;
 import kr.ac.tukorea.ge.n2016180022.dungeonndeffence.framework.game.MainGame;
 import kr.ac.tukorea.ge.n2016180022.dungeonndeffence.framework.view.GameView;
 
@@ -14,8 +15,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        MainGame.get();
-        setContentView(new GameView(this, null));
+//        MainGame.get();
+        setContentView(R.layout.activity_main);
     }
 
     public void onBtnStart(View view) {
